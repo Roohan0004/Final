64d5c4757f3694876fc457d0e9678fb4f29cd851
l1f19bsse0004 <l1f19bsse000@ucp.edu.pk>
2025-02-18 21:05:06 +0500

    initial commit

---
+++ repo_config.sh
@@ -0,0 +1,8 @@
+git config --local user.name l1f19bsse0004
+git config --local user.email l1f19bsse000@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch .gitignore
+touch ./src/index.html ./styles/styles.css
+npm init -y
+echo "done configuration"
